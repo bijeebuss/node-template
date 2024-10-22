de3c4aa1f423bd686a54ee00e2fc603cf79f559b
Michael Welnick <michael@welnick.net>
2024-10-22 18:15:12 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# document-upload-script
+
+To install dependencies:
+
+```bash
+npm install
+```
+
+To run:
+
+```bash
+npm start
+```
---
+++ index.ts
@@ -0,0 +1,6 @@
+console.log("Hello, world!");
+const a = 1;
+
+await new Promise((resolve) => setTimeout(resolve, 1000));
+
+console.log("Goodbye, world!");
